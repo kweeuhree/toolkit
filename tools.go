0a78b58f9ab59edd273c636e39fd524ad382dde6
Nika Kolesnikova <firstnamenika@gmail.com>
2025-01-14 13:21:19 -0500

    feat: create uploading functions and add a test for UploadOneFile

--- tools.go
+++ tools.go
@@ -2,6 +2,13 @@ package toolkit
 
 import (
 	"crypto/rand" // cryptographically secure random number generator
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 const randomStrSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+"
@@ -9,7 +16,10 @@ const randomStrSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012
 // Tools is the type used to instantiate this module.
 // Any variable of this type will have access to all the methods
 // with the receiver *Tools.
-type Tools struct{}
+type Tools struct {
+	MaxFileSize      int      // Specify the max size of a file permitted for uploading
+	AllowedFileTypes []string // Specify the file types to be permitted for uploading
+}
 
 // RandomString() takes in an integer that defines length of random string.
 // It uses randomStrSource as the source for the return string.
@@ -33,4 +43,150 @@ func (t *Tools) RandomString(n int) string {
 
 	// Convert the slice of runes into a string and return it.
 	return string(str)
+}
+
+// UploadedFile is a struct used to save information about an uploaded file
+type UploadedFile struct {
+	NewFileName      string
+	OriginalFileName string
+	FileSize         int64
+}
+
+// UploadOneFile is a convenience method that calls UploadFiles
+// Expectes only one file to be uploaded
+func (t *Tools) UploadOneFile(r *http.Request, uploadDir string, rename ...bool) (*UploadedFile, error) {
+	renameFile := true
+	if len(rename) > 0 {
+		renameFile = rename[0]
+	}
+
+	files, err := t.UploadFiles(r, uploadDir, renameFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return files[0], nil
+}
+
+// UploadFiles uploads one or more file to a specified directory, and gives the files a random name
+// Returns a slice of with the newly named files, the original file names, file sizes, and
+// a potential error. If the optional last parameter is set to true, the files will not be renamed
+func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) ([]*UploadedFile, error) {
+	// Rename by default
+	renameFile := true
+
+	// If the length of the variadic parameter is not zero, update renameFile
+	if len(rename) > 0 {
+		renameFile = rename[0]
+	}
+
+	// Preallocate a slice to store the files
+	var uploadedFiles []*UploadedFile
+
+	// Assign MaxFileSize if it is not set
+	if t.MaxFileSize == 0 {
+		// Set a default limit
+		t.MaxFileSize = 1024 * 1024 * 1024
+	}
+
+	// Check for an error when parsing the request
+	err := r.ParseMultipartForm(int64(t.MaxFileSize))
+	if err != nil {
+		return nil, errors.New("the uploaded file is too big")
+	}
+
+	// Check if any files are stored in the request
+	for _, headers := range r.MultipartForm.File {
+		for _, hdr := range headers {
+			// Wrap defer in a function
+			uploadedFiles, err = func(UploadedFiles []*UploadedFile) ([]*UploadedFile, error) {
+				var uploadedFile UploadedFile
+				// Open the header
+				infile, err := hdr.Open()
+				if err != nil {
+					return nil, err
+				}
+				// Close in order to avoid resource leak
+				defer infile.Close()
+
+				// We need to look at the first 512 bytes to find out the type of file
+				buff := make([]byte, 512)
+				_, err = infile.Read(buff) // Read the bytes
+				if err != nil {
+					return nil, err
+				}
+
+				// Check to see if the file type is permitted
+				// Assume that the file type is not allowed
+				allowed := false
+				fileType := http.DetectContentType(buff) // Get file type of the bytes
+
+				// Check if the AllowedFileTypes was populated
+				if len(t.AllowedFileTypes) > 0 {
+					for _, f := range t.AllowedFileTypes {
+						// If current file type equals one of the permitted file types...
+						if strings.EqualFold(fileType, f) {
+							// ...allow the file
+							allowed = true
+						}
+					}
+					// if AllowedFileTypes was not populated...
+				} else {
+					// ...allow all files
+					allowed = true
+				}
+
+				// If allowed is still false, return an error
+				if !allowed {
+					return nil, errors.New("the uploaded file type is not permitted")
+				}
+
+				// Since we read the beginning of the file,
+				// We have to go back to the beginning of the file
+				_, err = infile.Seek(0, 0)
+				if err != nil {
+					return nil, err
+				}
+
+				// If its going to be renamed - generate a new name with original extension
+				if renameFile {
+					uploadedFile.NewFileName = fmt.Sprintf("%s%s", t.RandomString(25), filepath.Ext(hdr.Filename))
+				} else {
+					uploadedFile.NewFileName = hdr.Filename
+				}
+
+				uploadedFile.OriginalFileName = hdr.Filename
+
+				// Save to disk
+				var outfile *os.File  // file we will write to
+				defer outfile.Close() // close the file when the function exists
+
+				// Write the file to the provided directory
+				if outfile, err = os.Create(filepath.Join(uploadDir, uploadedFile.NewFileName)); err != nil {
+					return nil, err
+				} else {
+					fileSize, err := io.Copy(outfile, infile)
+					if err != nil {
+						return nil, err
+					}
+
+					uploadedFile.FileSize = fileSize
+				}
+
+				// Append the file to the slice of uploadedFiles
+				uploadedFiles = append(uploadedFiles, &uploadedFile)
+
+				return uploadedFiles, nil
+
+				// give the function access to uploadedFiles
+			}(uploadedFiles)
+
+			// In case of error, return what was successfully uploaded
+			if err != nil {
+				return uploadedFiles, err
+			}
+		}
+	}
+
+	return uploadedFiles, nil
 }
--- tools_test.go
+++ tools_test.go
@@ -1,6 +1,17 @@
 package toolkit
 
-import "testing"
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"sync"
+	"testing"
+)
 
 func TestTools_RandomString(t *testing.T) {
 	var testTools Tools
@@ -21,6 +32,96 @@ func TestTools_RandomString(t *testing.T) {
 			if len(s) != entry.length {
 				t.Errorf("expected length %d, received %d", entry.length, len(s))
 			}
+		})
+	}
+}
+
+func TestTools_UploadOneFile(t *testing.T) {
+	tests := []struct {
+		name          string
+		allowedTypes  []string
+		renameFile    bool
+		errorExpected bool
+	}{
+		{"allowed no rename", []string{"image/jpeg", "image/png"}, false, false},
+	}
+
+	for _, entry := range tests {
+		t.Run(entry.name, func(t *testing.T) {
+			// Set up a pipe to avoid buffering while simulating a multipart file upload
+			pr, pw := io.Pipe()
+			// Create a multipart writer
+			mpWriter := multipart.NewWriter(pw)
+
+			// execute a goroutine in the background
+			// use a wait group to ensure that things occur in a particular sequence
+			wg := sync.WaitGroup{}
+			// Add one to a wait group
+			wg.Add(1)
+
+			// Create a go func to run concurrently with the program
+			go func() {
+				// Close the writer when the function is finished
+				defer mpWriter.Close()
+				// Decrement the wait group by one when the function is finished
+				defer wg.Done()
+
+				// Create the form data field 'file'
+				part, err := mpWriter.CreateFormFile("file", "./testdata/img.png")
+				if err != nil {
+					t.Error(err)
+				}
+
+				file, err := os.Open("./testdata/img.png")
+				if err != nil {
+					t.Error(err)
+				}
+
+				// Close the file to avoid resource leaks
+				defer file.Close()
+
+				// Decode the image
+				img, _, err := image.Decode(file)
+				if err != nil {
+					t.Error("error decoding image", err)
+				}
+
+				err = png.Encode(part, img)
+			}()
+
+			// Read from the pipe which receives data
+			// Create a request with a pipe reader
+			req := httptest.NewRequest(http.MethodPost, "/", pr)
+			// Set the correct content type for whatever type the payload is
+			req.Header.Add("Content-Type", mpWriter.FormDataContentType())
+
+			var testTools Tools
+			testTools.AllowedFileTypes = entry.allowedTypes
+			// Call UploadFiles with the pipe reader request, save to 'uploads' folder
+			uploadedFiles, err := testTools.UploadFiles(req, "./testdata/uploads/", entry.renameFile)
+			// Fail the test if the error is not nil and was not expected
+			if err != nil && !entry.errorExpected {
+				t.Errorf("expected no error, but received %+v", err)
+			}
+
+			// Build a string with the new file name
+			fileStr := fmt.Sprintf("./testdata/uploads/%s", uploadedFiles[0].NewFileName)
+
+			// Check if the file did not get uploaded
+			if !entry.errorExpected {
+				if _, err := os.Stat(fileStr); os.IsNotExist(err) {
+					t.Errorf("expected file to exist: %s", err.Error())
+				}
+
+				// Clean up
+				_ = os.Remove(fileStr)
+			}
+
+			// If error is expected and not received, log it
+			if entry.errorExpected && err == nil {
+				t.Errorf("expected an error, but none received")
+			}
+
 		})
 	}
 }
