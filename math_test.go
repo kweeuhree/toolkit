cea882724d517113df82b722edd4eddb446e7b3e
Nika Kolesnikova <firstnamenika@gmail.com>
2025-02-11 08:45:40 -0500

    update(math_test): ensure that Sum function is exported and add more test cases

--- math_test.go
+++ math_test.go
@@ -2,7 +2,7 @@ package toolkit
 
 import "testing"
 
-func Test_sum(t *testing.T) {
+func Test_Sum(t *testing.T) {
 	var tools Tools
 	tests := []struct {
 		name     string
@@ -11,15 +11,17 @@ func Test_sum(t *testing.T) {
 	}{
 		{"Small ints", []int{1, 2, 3, 4, 5}, 15},
 		{"Mixed ints", []int{1, 200, 3, 499, 5}, 708},
-		{"Small negative ints", []int{1, -2, -3, 4, 5}, 5},
+		{"Small negative ints", []int{-1, -2, -3, 4, -5}, -7},
 		{"Mixed negative ints", []int{1, -200, 3, 499, 5}, 308},
 		{"Big ints", []int{1111, 200, 3333, 499, 5555}, 10698},
+		{"Big mixed ints", []int{-1522, 2777733333, -3242411, 4432224, -1117895}, 2777803729},
+		{"Negative big ints", []int{-1111, -2400, -3333, -499, -5555}, -12898},
 		{"Zero", []int{0}, 0},
 	}
 
 	for _, entry := range tests {
 		t.Run(entry.name, func(t *testing.T) {
-			result := tools.sum(entry.nums)
+			result := tools.Sum(entry.nums)
 
 			if result != entry.expected {
 				t.Errorf("expected %d, received %d", entry.expected, result)
